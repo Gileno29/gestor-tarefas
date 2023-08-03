eaa1b287b52f9f77f5f2fc18372204009cba88a3
Gileno <gileno@tsmx.net.br>
2023-08-03 08:30:06 -0300

    added getall function

--- models/insert.go
+++ models/insert.go
@@ -12,5 +12,5 @@ func Insert(todo Todo) (id int64, err error) {
 	sql := `INSERT INTO todos (title, description, done) VALUES($1,$2,$3) RETURNING id`
 	err = conn.QueryRow(sql, todo.Title, todo.Description, todo.Done).Scan(&id)
 
-	return
+	return 
 }
